cec2ae1fcbb84185cb886161797c2ef43d89998b
i-m-abbhay <20je0018@me.iitism.ac.in>
2025-07-17 14:32:52 +0900

    Initial Commit with trying to use Square, Text and Triangle classes. experimenting with to_Edge, shift etc method

---
+++ ManimCourse.py
@@ -0,0 +1,21 @@
+from manim import *
+
+class Pith(Scene):
+    def construct(self):
+        sq = Square(side_length=5, stroke_color=GREEN, fill_color=BLUE, fill_opacity=0.75)
+        self.play(Create(sq), run_time = 3)
+        self.wait()
+
+
+class Testing(Scene):
+    def construct(self):
+        name = Text("Abhay").to_edge(UL, buff=0.5)
+        sq = Square(side_length=0.5).shift(LEFT * 3)
+        tri = Triangle().scale(0.6).to_edge(DR)
+        
+        self.play (Write(name))
+        self.play(DrawBorderThenFill(sq), run_time=2)
+        self.play (Create(tri))
+        self.wait()
+
+        self.play()
